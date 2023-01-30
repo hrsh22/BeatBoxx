29d883a413096449ad728da62fdc8b552f79e3fc
Ayush Shete <ayushete02@gmail.com>
2023-01-30 17:05:45 +0530

    Uload Button and Form

--- components/upload/uploadpage.js
+++ components/upload/uploadpage.js
@@ -1,11 +1,141 @@
-import React from 'react';
+import React, { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import {
+  Form,
+  Input,
+  Button,
+  Radio,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+  Checkbox,
+  Upload,
+  Modal,
+} from "antd";
+
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
 
 const UploadPage = () => {
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState("Content of the modal");
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = () => {
+    setModalText("The modal will be closed after two seconds");
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setOpen(false);
+  };
+
   return (
-    <div>
-      
+    <div className="ml-64 mt-24">
+      <Button type="primary" onClick={showModal}>
+        Upload File
+      </Button>
+      <Modal
+        title="Title"
+        open={open}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <p>
+          <Form
+            labelCol={{ span: 4 }}
+            wrapperCol={{ span: 14 }}
+            layout="horizontal"
+            style={{ maxWidth: 600 }}
+          >
+           
+           <Form.Item label="Upload" valuePropName="fileList">
+              <Upload action="/upload.do" listType="picture-card" >
+                <div>
+                  <PlusOutlined />
+                  <div style={{ marginTop: 8 }}>Upload Music</div>
+                </div>
+              </Upload>
+            </Form.Item>
+
+            <Form.Item label="Radio">
+              <Radio.Group>
+                <Radio value="apple"> Apple </Radio>
+                <Radio value="pear"> Pear </Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item label="Input">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Select">
+              <Select>
+                <Select.Option value="demo">Demo</Select.Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="TreeSelect">
+              <TreeSelect
+                treeData={[
+                  {
+                    title: "Light",
+                    value: "light",
+                    children: [{ title: "Bamboo", value: "bamboo" }],
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="Cascader">
+              <Cascader
+                options={[
+                  {
+                    value: "zhejiang",
+                    label: "Zhejiang",
+                    children: [
+                      {
+                        value: "hangzhou",
+                        label: "Hangzhou",
+                      },
+                    ],
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="DatePicker">
+              <DatePicker />
+            </Form.Item>
+            <Form.Item label="RangePicker">
+              <RangePicker />
+            </Form.Item>
+            <Form.Item label="InputNumber">
+              <InputNumber />
+            </Form.Item>
+            <Form.Item label="TextArea">
+              <TextArea rows={4} />
+            </Form.Item>
+            <Form.Item label="Switch" valuePropName="checked">
+              <Switch />
+            </Form.Item>
+           
+            <Form.Item label="Button">
+              <Button>Button</Button>
+            </Form.Item>
+          </Form>
+        </p>
+      </Modal>
     </div>
   );
-}
+};
 
 export default UploadPage;
