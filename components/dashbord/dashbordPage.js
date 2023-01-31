751f8786b6daf100c298baa3daf8ca30b5797018
Ayush Shete <ayushete02@gmail.com>
2023-01-31 14:05:49 +0530

    Corosol Dashbord Created

--- components/dashbord/dashbordPage.js
+++ components/dashbord/dashbordPage.js
@@ -1,83 +1,183 @@
-import React from 'react'
-
+import React from "react";
 
 const DashbordPage = () => {
   return (
+    <div className="ml-64 mt-16 bg-[#1f1f1f]">
     
-    <div className='ml-64 mt-16'>
-<div class="flex flex-wrap -m-3"> 
-  
-  
-  <div class="w-1/5 sm:w-1/2 md:w-1/3 flex flex-col p-3">
-    <div class="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
-      <img class="bg-cover h-48" src="https://images.unsplash.com/photo-1523978591478-c753949ff840?w=900;"></img>
-      <div class="p-4 flex-1 flex flex-col" >
-        <h3 class="mb-4 text-2xl">My heading</h3>
-        <div class="mb-4 text-grey-darker text-sm flex-1">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+      <div class="flex flex-col m-auto p-auto">
+        <h1 class="flex py-5  font-bold text-4xl text-gray-800"></h1>
+        <div class="flex overflow-x-scroll pb-10 scrollbar-hide">
+          <div class="flex flex-nowrap ">
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-400 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://www.svarenwebstudio.com/wp-content/themes/carbonate-master/images/free-seo-report.png" alt="" />
+                <i className="fas fa-play text-end p-3 ml-3 -mt-4 rounded-full bg-red-700"></i>
+                <h1 className="text-center font-bold -mt-6">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+          </div>
         </div>
-        <a href="#" class="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide" >Twitter</a>
       </div>
-    </div>  
-  </div>
-  
-  <div class="w-1/5 sm:w-1/2 md:w-1/3 flex flex-col p-3">
-    <div class="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
-      <img class="bg-cover h-48" src="https://images.unsplash.com/photo-1523978591478-c753949ff840?w=900;"></img>
-      <div class="p-4 flex-1 flex flex-col" >
-        <h3 class="mb-4 text-2xl">My heading</h3>
-        <div class="mb-4 text-grey-darker text-sm flex-1">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-        </div>
-        <a href="#" class="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide" >Twitter</a>
-      </div>
-    </div>  
-  </div>
-  
-  <div class="w-1/5 sm:w-1/2 md:w-1/3 flex flex-col p-3">
-    <div class="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
-      <img class="bg-cover h-48" src="https://images.unsplash.com/photo-1523978591478-c753949ff840?w=900;"></img>
-      <div class="p-4 flex-1 flex flex-col" >
-        <h3 class="mb-4 text-2xl">My heading</h3>
-        <div class="mb-4 text-grey-darker text-sm flex-1">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-        </div>
-        <a href="#" class="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide" >Twitter</a>
-      </div>
-    </div>  
-  </div>
-  
-  <div class="w-1/5 sm:w-1/2 md:w-1/3 flex flex-col p-3">
-    <div class="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
-      <img class="bg-cover h-48" src="https://images.unsplash.com/photo-1523978591478-c753949ff840?w=900;"></img>
-      <div class="p-4 flex-1 flex flex-col" >
-        <h3 class="mb-4 text-2xl">My heading</h3>
-        <div class="mb-4 text-grey-darker text-sm flex-1">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-        </div>
-        <a href="#" class="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide" >Twitter</a>
-      </div>
-    </div>  
-  </div>
-  
-  <div class="w-1/4 sm:w-1/2 md:w-1/3 flex flex-col p-3">
-    <div class="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
-      <img class="bg-cover h-48" src="https://images.unsplash.com/photo-1523978591478-c753949ff840?w=900;"></img>
-      <div class="p-4 flex-1 flex flex-col" >
-        <h3 class="mb-4 text-2xl">My heading</h3>
-        <div class="mb-4 text-grey-darker text-sm flex-1">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-        </div>
-        <a href="#" class="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide" >Twitter</a>
-      </div>
-    </div>  
-  </div>
 
-  
-    
-</div>
+      <div class="flex flex-col m-auto p-auto">
+        <h1 class="flex py-5  font-bold text-4xl text-gray-800"></h1>
+        <div class="flex overflow-x-scroll pb-10 scrollbar-hide">
+          <div class="flex flex-nowrap ">
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-col m-auto p-auto">
+        <h1 class="flex py-5  font-bold text-4xl text-gray-800"></h1>
+        <div class="flex overflow-x-scroll pb-10 scrollbar-hide">
+          <div class="flex flex-nowrap ">
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+            <div class="inline-block px-3">
+              <div class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img src="https://cdn3.iconfinder.com/data/icons/repairman-1/500/yul772_15_bath_tiles_logo_spa_water_house_construction-512.png" alt="" />
+                <h1 className="text-center font-bold">Headline</h1>
+                <h3 className="text-center">Description</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
 export default DashbordPage;
--- components/dashbord/index.js
+++ components/dashbord/index.js
@@ -5,10 +5,10 @@ import Dashbord from './dashbordPage';
 
 const DashbordLayout = () => {
   return (
-    <div>
+    <div className=''>
         <Sidebar className="relative"/>
         <Header classname="z-10"/>
-        <main class="bg-[#1f1f1f] flex-1 p-6 lg:px-8">
+        <main class=" flex-1 p-6 lg:px-8">
     <Dashbord/>
 
     </main>
