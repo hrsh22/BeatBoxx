c2322623419da465166fef8840c613b1984755bb
Ayush Shete <ayushete02@gmail.com>
2023-02-01 16:26:12 +0530

    Minor changes

--- components/metamask.js
+++ components/metamask.js
@@ -100,7 +100,7 @@ const MetaMask = () => {
                 <div
                 className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
               >
-                <i></i> {currentAccount.substring(0, 15)+'....'}
+                <i className="fas fa-user-shield"></i> {currentAccount.substring(0, 7)+'......'+currentAccount.substring(36, 42)}
               </div>
             )}
         
