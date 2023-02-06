482fd36adad5482b8461a744610df2e87148aa8b
Ayush Shete <ayushete02@gmail.com>
2023-02-06 20:24:56 +0530

    Revert "@Font remove"
    This reverts commit 99421f7fea79a67838f986578487b93327c9bee2.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import HomeLayout from '@/components/home'
 import { Alert, Space, Button } from 'antd';
