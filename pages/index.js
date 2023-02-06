c118218b5dc807628cbbf348f58fd89dbaa65790
Ayush Shete <ayushete02@gmail.com>
2023-02-06 20:23:05 +0530

    Revert "revert changes"
    This reverts commit 7d668d11b630a92d5668b45c3fc2a807faa3e148.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Head from "next/head";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import HomeLayout from '@/components/home'
 import { Alert, Space, Button } from 'antd';
 
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 const Index = () => {
   const [visible, setVisible] = useState(true);
